3a7443927b86b45b68506b247efa9c9737de0cc2
charlierobson <charlie.robson@b2m-solutions.com>
2016-12-07 13:39:55 +0000

    snapshot

--- valkyrie/Form1.Designer.cs
+++ valkyrie/Form1.Designer.cs
@@ -30,79 +30,60 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBoxURL = new System.Windows.Forms.TextBox();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.toolStripStatusLabel0 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.statusStrip1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBoxURL
             // 
             this.textBoxURL.AcceptsReturn = true;
             this.textBoxURL.AllowDrop = true;
-            this.textBoxURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxURL.Location = new System.Drawing.Point(13, 13);
+            this.textBoxURL.Location = new System.Drawing.Point(12, 295);
             this.textBoxURL.Name = "textBoxURL";
-            this.textBoxURL.Size = new System.Drawing.Size(286, 20);
+            this.textBoxURL.Size = new System.Drawing.Size(283, 20);
             this.textBoxURL.TabIndex = 0;
             this.textBoxURL.DragDrop += new System.Windows.Forms.DragEventHandler(this.OnDragDrop);
             this.textBoxURL.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
             this.textBoxURL.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxURL_KeyUp);
-            // 
-            // statusStrip1
-            // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel0,
-            this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 159);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(311, 22);
-            this.statusStrip1.SizingGrip = false;
-            this.statusStrip1.TabIndex = 1;
-            this.statusStrip1.Text = "statusStrip1";
-            // 
-            // toolStripStatusLabel1
-            // 
-            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
             // 
             // listBox1
             // 
             this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.DisplayMember = "DisplayString";
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(13, 40);
+            this.listBox1.Location = new System.Drawing.Point(302, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(286, 108);
+            this.listBox1.Size = new System.Drawing.Size(199, 303);
             this.listBox1.TabIndex = 2;
-            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            this.listBox1.ValueMember = "DisplayString";
             // 
-            // toolStripStatusLabel0
+            // pictureBox1
             // 
-            this.toolStripStatusLabel0.Name = "toolStripStatusLabel0";
-            this.toolStripStatusLabel0.Size = new System.Drawing.Size(0, 17);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(283, 274);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(311, 181);
+            this.ClientSize = new System.Drawing.Size(513, 327);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.textBoxURL);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(327, 220);
+            this.MinimumSize = new System.Drawing.Size(529, 366);
             this.Name = "Form1";
             this.Text = "Valkyrie";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.OnDragDrop);
-            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,10 +92,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBoxURL;
-        private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel0;
-        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- valkyrie/Form1.cs
+++ valkyrie/Form1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
-using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using VideoLibrary;
 
@@ -11,110 +11,118 @@ namespace valkyrie
 {
     public partial class Form1 : Form
     {
-        private readonly Thread _downloaderThread;
-        private readonly object _listLock;
-        private readonly Queue<string> _downloadQueue;
-        private bool _downloaderFinished;
-
         public Form1()
         {
             InitializeComponent();
-
-            _listLock = new object();
-            _downloadQueue = new Queue<string>();
-
-            _downloaderThread = new Thread(Downloader);
-            _downloaderThread.Start();
-
-            ShowDownloadCount();
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private async Task DownloadVideoById(string youTubeVideoID)
         {
-            _downloaderFinished = true;
-            _downloaderThread.Join();
-        }
-
-        private void ShowDownloadCount()
-        {
-            toolStripStatusLabel0.Text = _downloadQueue.Count.ToString();
-        }
-
-        private void SetStatus(string status)
-        {
-            toolStripStatusLabel1.Text = status;
-        }
-
-        private void Downloader()
-        {
-            while (!_downloaderFinished)
+            Invoke((MethodInvoker)delegate { listBox1.Items.Add(youTubeVideoID); });
+            await Task.Run(() =>
             {
-                if (_downloadQueue.Count == 0)
-                {
-                    Thread.Sleep(200);
-                    continue;
-                }
-
-                string downloadItem;
-                lock (_listLock)
-                {
-                    downloadItem = _downloadQueue.Dequeue();
-                }
-
-                Invoke((MethodInvoker) (() => SetStatus("Downloading...")));
-
-                const string patt =
-                    @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$";
-                var match = Regex.Match(downloadItem, patt);
-                if (match.Groups.Count <= 4) return;
-
-                var videoID = "https://youtube.com/watch?v=" + match.Groups[5];
-                var savedName = string.Empty;
-
                 try
                 {
+                    var videoID = "https://youtube.com/watch?v=" + youTubeVideoID;
+
                     using (var service = Client.For(YouTube.Default))
                     {
                         var video = service.GetVideo(videoID);
 
-                        savedName =
-                            Path.Combine(
+                        var targetName =
+                             Path.Combine(
                                 Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                                    "Downloads"), video.FullName);
-                        File.WriteAllBytes(savedName, video.GetBytes());
+                                    "Downloads"), video.FullName).ToLowerInvariant();
 
-                        Invoke((MethodInvoker) (() => listBox1.Items.Add(savedName)));
+                        var destName = Path.ChangeExtension(targetName.Replace(" - youtube", string.Empty), "mp3");
+
+                        if (File.Exists(destName)) return;
+
+                        File.WriteAllBytes(targetName, video.GetBytes());
+
+                        var tool = new Process
+                        {
+                            StartInfo =
+                            {
+                                FileName = "ffmpeg.exe",
+                                Arguments = $"-i \"{targetName}\" -y -q:a 0 -map a \"{destName}\"",
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = true
+                            }
+                        };
+
+                        tool.Start();
+                        tool.WaitForExit();
+                        if (tool.ExitCode == 0)
+                        {
+                            File.Delete(targetName);
+                        }
                     }
                 }
-                catch (Exception ex)
+                catch
                 {
-                    Invoke((MethodInvoker) (() => listBox1.Items.Add("FAILED - " + savedName)));
-                    Console.WriteLine(ex);
+                    /**/
                 }
+            });
+            Invoke((MethodInvoker)delegate { listBox1.Items.RemoveAt(0); });
+        }
 
-                Invoke((MethodInvoker) (() => SetStatus(string.Empty)));
-                Invoke((MethodInvoker) (ShowDownloadCount));
+        private void QueueUpIdFromHtml(string htmlFilename)
+        {
+            var fileContent = File.ReadAllText(htmlFilename);
+
+            var idSet = new HashSet<string>();
+            var regex = new Regex(@"(?<=watch\?v=)(.*?)(?=&amp)");
+
+            var matches = regex.Matches(fileContent);
+            foreach (Match match in matches)
+            {
+                if (match.Value.Length == 11) idSet.Add(match.Value);
             }
+
+            Parallel.ForEach(idSet, new ParallelOptions { MaxDegreeOfParallelism = 4 }, id =>
+            {
+                DownloadVideoById(id);
+            });
+        }
+
+        private async void QueueUpIdFromUrl(string item)
+        {
+            const string patt = @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$";
+            var match = Regex.Match(item, patt);
+            if (match.Groups.Count <= 4) return;
+
+            var id = match.Groups[5].ToString();
+            await DownloadVideoById(id);
         }
 
         private void OnDragEnter(object sender, DragEventArgs e)
         {
-            e.Effect = e.Data.GetDataPresent(DataFormats.Text) ? DragDropEffects.Copy : DragDropEffects.None;
+            if (e.Data.GetDataPresent(DataFormats.Text) || e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
         }
 
         private void OnDragDrop(object sender, DragEventArgs e)
         {
-            if (!e.Data.GetDataPresent(typeof(string))) return;
             if (e.Effect != DragDropEffects.Copy) return;
 
-            var item = (string) e.Data.GetData(typeof(string));
-            Console.WriteLine(item);
-
-            lock (_listLock)
+            if (e.Data.GetDataPresent(DataFormats.Text))
             {
-                _downloadQueue.Enqueue(item);
+                var item = (string)e.Data.GetData(typeof(string));
+                QueueUpIdFromUrl(item);
             }
-            ShowDownloadCount();
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var filenames = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (var filename in filenames)
+                {
+                    QueueUpIdFromHtml(filename);
+                }
+            }
         }
 
         private void textBoxURL_KeyUp(object sender, KeyEventArgs e)
@@ -125,28 +133,9 @@ namespace valkyrie
 
             textBoxURL.Text = string.Empty;
 
-            lock (_listLock)
-            {
-                _downloadQueue.Enqueue(item);
-            }
-            ShowDownloadCount();
+            QueueUpIdFromUrl(item);
 
             e.Handled = true;
-        }
-
-        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            var index = listBox1.IndexFromPoint(e.Location);
-            if (index == ListBox.NoMatches) return;
-
-            try
-            {
-                Process.Start((string)listBox1.Items[index]);
-            }
-            catch
-            {
-                //
-            }
         }
     }
 }
